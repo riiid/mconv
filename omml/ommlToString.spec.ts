4a46bf41f05ea0b06a631edb36f660f6a05d75e9
Yongwook Choi <hyperflow@kakao.com>
2022-06-08 17:32:21 +0900

    lazy refactored code

--- omml/ommlToString.spec.ts
+++ omml/ommlToString.spec.ts
@@ -28,4 +28,73 @@ Deno.test("ommlToString", async (t) => {
       ],
     }),
   );
+
+  await assertSnapshot(
+    t,
+    ommlToString({
+      children: [
+        {
+          children: [
+            {
+              tagName: "m:t",
+              text: "23x + 9",
+              xmlSpace: undefined,
+            },
+          ],
+          rpr: undefined,
+          tagName: "m:r",
+        },
+        {
+          e: {
+            argpr: undefined,
+            children: [
+              {
+                children: [
+                  {
+                    tagName: "m:t",
+                    text: "x",
+                    xmlSpace: undefined,
+                  },
+                ],
+                rpr: undefined,
+                tagName: "m:r",
+              },
+            ],
+          },
+          ssuppr: {
+            tagName: "m:sSupPr",
+          },
+          sup: {
+            argpr: undefined,
+            children: [
+              {
+                children: [
+                  {
+                    tagName: "m:t",
+                    text: "9999",
+                    xmlSpace: undefined,
+                  },
+                ],
+                rpr: undefined,
+                tagName: "m:r",
+              },
+            ],
+          },
+          tagName: "m:sSup",
+        },
+        {
+          children: [
+            {
+              tagName: "m:t",
+              text: "= 3",
+              xmlSpace: undefined,
+            },
+          ],
+          rpr: undefined,
+          tagName: "m:r",
+        },
+      ],
+      tagName: "m:oMath",
+    }),
+  );
 });
